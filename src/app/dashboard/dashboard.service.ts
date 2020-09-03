8ac362457524b74dab146b8ca0dd93c7964d9960
ealvess <everton10alves@gmail.com>
2020-09-03 03:35:55 -0300

    Finalizando graficos de estatisticas

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -11,27 +11,34 @@ export class DashboardService {
 
   entradaMedicamentoUrl: string;
   entradaCorrelatoUrl: string;
+  saidaDeMedicamentoParaPacientes: string;
+  saidaDeMedicamentoParaCentroDeCusto: string;
+  saidaDeCorrelatosPorMes: string;
 
   constructor(private http: HttpClient) {
     this.entradaMedicamentoUrl = `${environment.apiUrl}/entradamedicamentos`;
+    this.saidaDeMedicamentoParaPacientes = `${environment.apiUrl}/saidamedicamentos`;
+    this.saidaDeMedicamentoParaCentroDeCusto = `${environment.apiUrl}/saidamedicamentoscentrodecusto`;
     this.entradaCorrelatoUrl = `${environment.apiUrl}/entradacorrelatos`;
+    this.saidaDeCorrelatosPorMes = `${environment.apiUrl}/saidacorrelato`;
   }
 
-  EntradasDeMedicamentoPorMedicamento(): Promise<Array<any>> {
+  EntradasDeMedicamentoPorMes(): Promise<Array<any>> {
     return this.http.get(`${this.entradaMedicamentoUrl}/estatisticas/por-medicamento`)
       .toPromise()
       .then(response => response as Array<any>);
   }
 
-  EntradasDeMedicamentoPorDia(): Promise<Array<any>> {
-    return this.http.get<Array<any>>(`${this.entradaMedicamentoUrl}/estatisticas/por-dia`)
+  SaidaDeMedicamentoPorMesParaPacientes(): Promise<Array<any>> {
+    return this.http.get(`${this.saidaDeMedicamentoParaPacientes}/estatisticas/por-mes`)
       .toPromise()
-      .then(response => {
-        const dados = response;
-        this.converterStringsParaDatas(dados);
+      .then(response => response as Array<any>);
+  }
 
-        return dados;
-      });
+  SaidaDeMedicamentoPorMesParaCentroDeCusto(): Promise<Array<any>> {
+    return this.http.get(`${this.saidaDeMedicamentoParaCentroDeCusto}/estatisticas/por-mes`)
+      .toPromise()
+      .then(response => response as Array<any>);
   }
 
   EntradasDeCorrelatoPorMes(): Promise<Array<any>> {
@@ -40,9 +47,9 @@ export class DashboardService {
       .then(response => response as Array<any>);
   }
 
-  private converterStringsParaDatas(dados: Array<any>) {
-    for (const dado of dados) {
-      dado.dia = moment(dado.dia, 'YYYY-MM-DD').toDate();
-    }
+  SaidaDeCorrelatoPorMes(): Promise<Array<any>> {
+    return this.http.get(`${this.saidaDeCorrelatosPorMes}/estatisticas/por-mes`)
+      .toPromise()
+      .then(response => response as Array<any>);
   }
 }
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -9,19 +9,24 @@ import { DashboardService } from '../dashboard.service';
 export class DashboardComponent implements OnInit {
 
   medicamentoPorMes: any;
+  saidaPacientes: any;
+  saidaCentroDeCusto:any;
   correlatosPorMes: any;
+  saidaCorrelatosPorMes:any;
   lineChartData: any;
 
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
-    this.configurarGraficoPizzaMedicamento();
-    this.configurarGraficoLinha();
+    this.configurarGraficoPizzaEntradaMedicamento();
+    this.configurarGraficoPizzaSaidaMedicamentoParaPacientesPorMes();
+    this.configurarGraficoPizzaSaidaMedicamentoParaCentroDeCustoPorMes();
     this.configurarGraficoPizzaCorrelato();
+    this.configurarGraficoPizzaSaidaCorrelato();
   }
 
-  configurarGraficoPizzaMedicamento() {
-    this.dashboardService.EntradasDeMedicamentoPorMedicamento()
+  configurarGraficoPizzaEntradaMedicamento() {
+    this.dashboardService.EntradasDeMedicamentoPorMes()
       .then(dados => {
         this.medicamentoPorMes = {
           labels: dados.map(dado => dado.medicamento.nome),
@@ -39,40 +44,47 @@ export class DashboardComponent implements OnInit {
       });
   }
 
-  configurarGraficoLinha() {
-    this.dashboardService.EntradasDeMedicamentoPorDia()
-      .then(dados => {
-        console.log('grafico dias', dados);
-        const diasDoMes = this.configurarDiasMes();
-        const totaisMedicamentos = this.totaisPorCadaDiaMes(dados.filter(
-          dado => dado.medicamento.nome === 'HEXOMEDINE'), diasDoMes);
-        const totaisMedicamentoss = this.totaisPorCadaDiaMes(dados.filter(
-          dado => dado.medicamento.nome === 'DIPIRONA 500MG'), diasDoMes);
-        console.log('teste', totaisMedicamentos);
-
-        this.lineChartData = {
-          labels: diasDoMes,
+  configurarGraficoPizzaSaidaMedicamentoParaPacientesPorMes() {
+    this.dashboardService.SaidaDeMedicamentoPorMesParaPacientes()
+      .then(dados => {        
+        this.saidaPacientes = {
+          labels: dados.map(dado => dado.medicamento.medicamento.nome),
           datasets: [
             {
-              label: 'HEXO',
-              data: totaisMedicamentos,
-              backgroundColor: '#42A5F5',
-              borderColor: '#1E88E5',
-            },
-            {
-              label: 'DIPI',
-              data: totaisMedicamentoss,
-              backgroundColor: '#9CCC65',
-              borderColor: '#7CB342',
+              data: dados.map(dado => dado.quantidade),
+              backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6',
+                '#DD4477', '#3366CC', '#DC3912', '#be0027', '#ffdd00', '#279b37', '#52325d',
+                '#00a78e', '#0cb9c1', '##ff9933', '#b4a996', '#000000', '#00395d', '#0033a1',
+                '#84754e', '#f9e498', '#ff7243', '#ff4816', '#f65a5b', '#d2ea32', '#2db928',
+                '#30660f', '#ae9a64', '#606061', '#a3a7a6', '#ff0092', '#cd595a', '#ffaaaa']
             }
           ]
-        }
+        };
+      });
+  }
+
+  configurarGraficoPizzaSaidaMedicamentoParaCentroDeCustoPorMes() {
+    this.dashboardService.SaidaDeMedicamentoPorMesParaPacientes()
+      .then(dados => {
+        this.saidaCentroDeCusto = {
+          labels: dados.map(dado => dado.medicamento.medicamento.nome),
+          datasets: [
+            {
+              data: dados.map(dado => dado.quantidade),
+              backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6',
+                '#DD4477', '#3366CC', '#DC3912', '#be0027', '#ffdd00', '#279b37', '#52325d',
+                '#00a78e', '#0cb9c1', '##ff9933', '#b4a996', '#000000', '#00395d', '#0033a1',
+                '#84754e', '#f9e498', '#ff7243', '#ff4816', '#f65a5b', '#d2ea32', '#2db928',
+                '#30660f', '#ae9a64', '#606061', '#a3a7a6', '#ff0092', '#cd595a', '#ffaaaa']
+            }
+          ]
+        };
       });
   }
 
   configurarGraficoPizzaCorrelato() {
     this.dashboardService.EntradasDeCorrelatoPorMes()
-      .then(dados => {
+      .then(dados => {        
         this.correlatosPorMes = {
           labels: dados.map(dado => dado.correlato.nome),
           datasets: [
@@ -89,40 +101,23 @@ export class DashboardComponent implements OnInit {
       });
   }
 
-  private totaisPorCadaDiaMes(dados, diasDoMes) {
-    const totais: number[] = [];
-    for (const dia of diasDoMes) {
-      let total = 0;
-
-      for (const dado of dados) {
-        if (dado.dia.getDate() === dia) {
-          total = dado.total;
-
-          break;
-        }
-      }
-
-      totais.push(total);
-    }
-
-    return totais;
+  configurarGraficoPizzaSaidaCorrelato() {
+    this.dashboardService.SaidaDeCorrelatoPorMes()
+      .then(dados => {
+        this.saidaCorrelatosPorMes = {
+          labels: dados.map(dado => dado.correlato.correlato.nome),
+          datasets: [
+            {
+              data: dados.map(dado => dado.quantidade),
+              backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6',
+                '#DD4477', '#3366CC', '#DC3912', '#be0027', '#ffdd00', '#279b37', '#52325d',
+                '#00a78e', '#0cb9c1', '##ff9933', '#b4a996', '#000000', '#00395d', '#0033a1',
+                '#84754e', '#f9e498', '#ff7243', '#ff4816', '#f65a5b', '#d2ea32', '#2db928',
+                '#30660f', '#ae9a64', '#606061', '#a3a7a6', '#ff0092', '#cd595a', '#ffaaaa']
+            }
+          ]
+        };
+      });
   }
-
-  private configurarDiasMes() {
-    const mesReferencia = new Date();
-    mesReferencia.setMonth(mesReferencia.getMonth() + 1);
-    mesReferencia.setDate(0);
-
-    const quantidade = mesReferencia.getDate();
-
-    const dias: number[] = [];
-
-    for (let i = 1; i <= quantidade; i++) {
-      dias.push(i);
-    }
-
-    return dias;
-  }
-
 
 }
